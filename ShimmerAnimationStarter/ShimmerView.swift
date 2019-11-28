37f12926c91f08af6cf2835cd15f163d23f46d64
Vasudha Jha <jhav2@INITJHAV2M2.corp.emc.com>
2019-11-28 15:03:11 +0530

    First commit

---
+++ ShimmerAnimationStarter/ShimmerView.swift
@@ -0,0 +1,14 @@
+//
+//  ShimmerView.swift
+//  ShimmerAnimationStarter
+//
+//  Created by Jha, Vasudha on 28/11/19.
+//  Copyright © 2019 Jha, Vasudha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShimmerView: UIView {
+    
+}
---
+++ ShimmerAnimationStarter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ShimmerAnimationStarter
+//
+//  Created by Jha, Vasudha on 28/11/19.
+//  Copyright © 2019 Jha, Vasudha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: ShimmerView!
+    @IBOutlet weak var nameContainerView: ShimmerView!
+    @IBOutlet weak var emailContainerView: ShimmerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
